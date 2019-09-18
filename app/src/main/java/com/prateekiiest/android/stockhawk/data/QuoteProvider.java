c3e298a3fcb88d4c495436b2f070d0dd1a6b3ca9
Prateek Chanda <prateekkol21@gmail.com>
2019-09-18 22:32:41 +0530

    Update QuoteProvider.java

--- app/src/main/java/com/prateekiiest/android/stockhawk/data/QuoteProvider.java
+++ app/src/main/java/com/prateekiiest/android/stockhawk/data/QuoteProvider.java
@@ -12,7 +12,7 @@ import net.simonvt.schematic.annotation.TableEndpoint;
  */
 @ContentProvider(authority = QuoteProvider.AUTHORITY, database = QuoteDatabase.class)
 public class QuoteProvider {
-    public static final String AUTHORITY = "com.sam_chordas.android.stockhawk.data.QuoteProvider";
+    public static final String AUTHORITY = "com.prateekiiest.android.stockhawk.data.QuoteProvider";
 
     static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
